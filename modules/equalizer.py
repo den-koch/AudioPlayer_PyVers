64da2354b1576060b6580b2e79b9b5ca01d67cac
Denys Kochetov <125379298+den-koch@users.noreply.github.com>
2023-04-29 01:02:19 +0300

    Create equalizer.py

---
+++ modules/equalizer.py
@@ -0,0 +1,31 @@
+"""
+Equalizer class
+"""
+
+import random
+from PyQt5.QtWidgets import QWidget, QGridLayout
+from PyQt5 import QtCore
+from PyQt5.Equalizer import EqualizerBar
+
+
+class MyEqualizer(QWidget):
+    """ Equalizer"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.equalizer = EqualizerBar(5, ['#0C0786', '#40039C', '#6A00A7', '#8F0DA3', '#B02A8F', '#CA4678', '#E06461',
+                                          '#F1824C', '#FCA635', '#FCCC25', '#EFF821'])
+        self.equalizer.setBackgroundColor("transparent")
+
+        self.grid_layout = QGridLayout(self)
+        self.grid_layout.addWidget(self.equalizer)
+
+        self.set_timer = QtCore.QTimer()
+        self.set_timer.setInterval(100)
+        self.set_timer.timeout.connect(self.update_values)
+
+    def update_values(self):
+        self.equalizer.setValues([
+            min(100, value + random.randint(0, 50) if random.randint(0, 5) > 2 else value)
+            for value in self.equalizer.values()])
