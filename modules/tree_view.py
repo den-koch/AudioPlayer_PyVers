f750265bad8eb798450faee090309fa17ed91882
Denys Kochetov <125379298+den-koch@users.noreply.github.com>
2023-05-01 00:54:15 +0300

    Create tree_view.py

---
+++ modules/tree_view.py
@@ -0,0 +1,15 @@
+""" Tree view settings"""
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QTreeView
+from main import *
+
+
+class MyTreeView(QTreeView):
+
+    def mousePressEvent(self, event):
+
+        if event.button() == QtCore.Qt.RightButton:
+            self.clearSelection()
+        else:
+            print("left click !")
+        QTreeView.mousePressEvent(self, event)
