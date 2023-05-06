c8575784c3e349c62d749f9b10f3170bf1c678be
Denys Kochetov <125379298+den-koch@users.noreply.github.com>
2023-05-07 01:17:16 +0300

    Create settings.py

---
+++ widgets/settings.py
@@ -0,0 +1,5 @@
+"""Menu Style Sheet part for Open Button """
+
+file_filter_files: tuple = ("*.mp3", "*.mp4", "*.wav", "*.m4a", "*.flac", "*.wma")
+
+file_filter_dir: tuple = (".mp3", ".mp4", ".wav", ".m4a", ".flac", ".wma")
