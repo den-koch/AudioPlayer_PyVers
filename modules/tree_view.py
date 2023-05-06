7d9a7fb05640af0189d09e4769e62b84e7798126
Denys Kochetov <125379298+den-koch@users.noreply.github.com>
2023-05-06 18:08:01 +0300

    Update tree_view.py

--- modules/tree_view.py
+++ modules/tree_view.py
@@ -1,15 +1,19 @@
 """ Tree view settings"""
 from PyQt5 import QtCore
 from PyQt5.QtWidgets import QTreeView
-from main import *
 
 
 class MyTreeView(QTreeView):
 
     def mousePressEvent(self, event):
-
         if event.button() == QtCore.Qt.LeftButton:
             self.clearSelection()
-        else:
-            print("left click !")
+        elif event.button() == QtCore.Qt.RightButton:
+            index = self.indexAt(event.pos())
+            if not index.isValid():
+                return
+            if index.parent().data() is None:
+                self.edit(index)
+                self.setCurrentIndex(index)
+
         QTreeView.mousePressEvent(self, event)
